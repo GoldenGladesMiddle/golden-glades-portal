f29c27cfa73dd572f9f2cd5b0165c5859d633f5d
Kel <75871283+d4rk-kel@users.noreply.github.com>
2026-08-15 10:30:33 -0400

    Fix formatting in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Golden Glades Middle",
   description: "Golden Glades Middle School",
   icons: {
-    icon: "/logo.png",
+    icon: "/logo.png", 
   },
 };
 

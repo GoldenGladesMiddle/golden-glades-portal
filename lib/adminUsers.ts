146e42bb8705eee120f2f7934917a120670b2926
Kel <75871283+d4rk-kel@users.noreply.github.com>
2026-08-16 10:40:40 -0400

    Add ADMIN_USERS constant for admin credentials

---
+++ lib/adminUsers.ts
@@ -0,0 +1,4 @@
+export const ADMIN_USERS = [
+  { username: 'director', password: 'DirectorPassword2026!', name: 'School Director' },
+  { username: 'admin1', password: 'AdminPassword123!', name: 'Assistant Admin' },
+];
